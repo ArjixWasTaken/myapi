20c08ef4410018dd5fb78aaf4d6cb6f18e001ea9
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-06-24 23:06:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
 import requests, json, anilist, yify
-
+from flask import Response, render_template
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,32 +13,29 @@ class AnimeINFO(Resource):
 class MangaINFO(Resource):
 	def get(self, query):
 		return jsonify(anilist.search_anilist(query, type='MANGA'))
+
 class Yify(Resource):
 	def get(self, query):
-		return yify.search_yify(query)
+		return jsonify(yify.search_yify(query))
+
 class Home(Resource):
 	def get(self):
 		resources = {
 			"AnimeINFO": {
-				"parameter": "/animeinfo/<query>",
-				"example": "/animeinfo/overlord",
-				"help": 'Performs a search using the provided query and returns up to 10 results fetched from AniList.'
+				"parameter": '/animeinfo/<query>',
+				"example": '/animeinfo/overlord',
+				"help": 'performs a search using the provided query and returns up to 10 results fetched from AniList.'
 			}, 
 			"MangaINFO": {
-				"parameter": "/mangainfo/<query>",
-				"example": "/mangainfo/overlord",
-				"help": 'Performs a search using the provided query and returns up to 10 results fetched from AniList.'
-			},
-			"Yify": {
-				"parameter": "/yify/<query>",
-				"example": "/yify/sonic the hedgehog",
-				"help": "Scrapes the website yts.mx and returns all the useful information."
+				"parameter": '/mangainfo/<query>',
+				"example": '/mangainfo/overlord',
+				"help": 'performs a search using the provided query and returns up to 10 results fetched from AniList.'
 			}
 		}
 		return resources
 
 
-
+#################################################
 api.add_resource(Home, '/')
 api.add_resource(Yify, '/yify/<query>')
 api.add_resource(AnimeINFO, '/animeinfo/<query>')
