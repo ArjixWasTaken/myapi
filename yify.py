3b968e82bedb31ad84e379809661bc1511fc0428
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-06-24 23:49:58 +0300

    Update yify.py

--- yify.py
+++ yify.py
@@ -38,5 +38,7 @@ def search_yify(query):
 
 
     except:
+        url = f'https://yts.mx/ajax/search?query={query}'
+        return response = get_json(url)
         results_list = [{'status': "error"}]
     return results_list
