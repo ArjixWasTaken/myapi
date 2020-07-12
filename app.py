532a16c76dcb2498818d3f4d5b580526efa58522
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-07-12 23:41:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
-import requests, json, anilist, yify
+import requests, json, anilist
 from flask import Response, render_template
 
 
@@ -31,11 +31,6 @@ class Home(Resource):
 				"parameter": '/mangainfo/<query>',
 				"example": '/mangainfo/overlord',
 				"help": 'performs a search using the provided query and returns up to 10 results fetched from AniList.'
-			},
-			"Yify": {
-				"parameter": '/yify/<query>',
-				"example": "/yify/sonic the hedgehog",
-				"help": "Scrapes the website yts.mx and returns all the useful information."
 			}
 		}
 		return resources
@@ -43,7 +38,6 @@ class Home(Resource):
 
 #################################################
 api.add_resource(Home, '/')
-api.add_resource(Yify, '/yify/<query>')
 api.add_resource(AnimeINFO, '/animeinfo/<query>')
 api.add_resource(MangaINFO, '/mangainfo/<query>')
 if __name__ == '__main__':
